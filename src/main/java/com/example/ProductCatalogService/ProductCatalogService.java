0ba7ac2329b2a29c1eeecd26f2e54d215efc5b2d
Anup Jambhekar <anupjambhekar@gmail.com>
2024-09-23 08:47:50 +0530

    Added Product Catalog Service

---
+++ src/main/java/com/example/ProductCatalogService/ProductCatalogService.java
@@ -0,0 +1,13 @@
+package com.example.ProductCatalogService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCatalogService.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/ProductCatalogService/models/Product.java
@@ -0,0 +1,25 @@
+package com.example.ProductCatalogService.models;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Product extends BaseModel {
+    private String title;
+
+    private String description;
+
+    private String imageUrl;
+
+    private Double amount;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Category category;
+
+    private Boolean isPrimeSpecific;
+}
